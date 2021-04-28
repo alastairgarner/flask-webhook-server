f4852c4c1bc48a534198ffba5b6795f01146fed8
Alastair Garner <alastairgarner@outlook.com>
2021-04-28 22:00:56 +0100

    added auto-formatting

--- rtmconnector.py
+++ rtmconnector.py
@@ -7,66 +7,70 @@ RTMMethods = {
     "get_tasks": "rtm.tasks.getList"
 }
 
+
 class RTMConnector(Rtm):
-    
-    _required_env = ["RTM_API_KEY","RTM_API_SECRET"]
-    
+
+    _required_env = ["RTM_API_KEY", "RTM_API_SECRET"]
+
     def __init__(self, perms="read", api_version=None, env_file='./.env'):
-        
+
         for var in self._required_env:
             if not var in os.environ:
-                raise Exception(f"Missing the {var} environment variable - please update .env")
-        
+                raise Exception(
+                    f"Missing the {var} environment variable - please update .env")
+
         api_key = os.environ["RTM_API_KEY"]
         shared_secret = os.environ["RTM_API_SECRET"]
         token = os.environ["RTM_API_TOKEN"]
-        
+
         self._env = env_file
-        
-        super().__init__(api_key, shared_secret, perms=perms, token=token, api_version=api_version)
+
+        super().__init__(api_key, shared_secret, perms=perms,
+                         token=token, api_version=api_version)
         self.check_connection()
-        
+
     def check_connection(self):
         """docstring"""
-        
+
         if not self.token_valid():
             self._update_connection()
-            
+
         print('Connection is good!')
-   
+
     def _update_connection(self):
         """
         docstring
         """
-        
-        url,frob = self.authenticate_desktop()
-        print(f"Follow the URL to authenticate with the Remember The Milk API:\n\n{url}\n")
-        
+
+        url, frob = self.authenticate_desktop()
+        print(
+            f"Follow the URL to authenticate with the Remember The Milk API:\n\n{url}\n")
+
         confirm = input("Confirm that you've authenticated (Y/n) \n")
         if confirm.lower() != 'y':
             print("Failure to authenicate with RTM may result in functionality problems")
             return
-            
+
         success = self.retrieve_token(frob)
         if not success:
             return
-        
+
         self._update_dotenv()
-        
+
     def _update_dotenv(self):
         """docstring"""
-        
+
         dotenv.set_key(self._env, "RTM_API_TOKEN", self.token)
         os.environ["RTM_API_TOKEN"] = self.token
-        
+
     def generate_url(self, method, **kwargs):
         return super()._make_request_url(method=method, api_key=self.api_key, auth_token=self.token, **kwargs)
-        
+
     def get(self, method, **kwargs):
         return super()._make_request(method=method, api_key=self.api_key, auth_token=self.token, **kwargs)
-        
+
     def get_subscriptions(self, **kwargs):
         return self.get(RTMMethods["get_subscriptions"], **kwargs)
-    
+
     def get_tasks(self, **kwargs):
         return self.get(RTMMethods["get_tasks"], **kwargs)
--- test_rtm_api.py
+++ test_rtm_api.py
@@ -1,4 +1,5 @@
 
+from flask import Flask, request, Response
 import os
 import dotenv
 import hashlib
@@ -9,29 +10,30 @@ from logging import Logger
 
 logger = logging.getLogger()
 
-from flask import Flask, request, Response
 app = Flask(__name__)
 
 # NOTE for API call to work, need to pass in the api_key and auth_token
 
+
 class RtmApi(object):
-    
+
     AUTH_URL = "https://api.rememberthemilk.com/services/auth/"
     BASE_URL = "https://api.rememberthemilk.com/services/rest/"
-    
-    _required_env = ["RTM_API_KEY","RTM_API_SECRET"]
-    
-    def __init__(self, logger:Logger, env_file:str=None) -> None:
-        
+
+    _required_env = ["RTM_API_KEY", "RTM_API_SECRET"]
+
+    def __init__(self, logger: Logger, env_file: str = None) -> None:
+
         for env in self._required_env:
             if env not in os.environ:
-                logger.error(f"%s RtmApi - failed to initialise: Missing environment variable {env}")
+                logger.error(
+                    f"%s RtmApi - failed to initialise: Missing environment variable {env}")
                 return
-        
+
         self.KEY = os.environ["RTM_API_KEY"]
         self.SECRET = os.environ["RTM_API_SECRET"]
         self.TOKEN = os.environ["RTM_API_TOKEN"]
-        
+
         self.BASE_HEADER = {
             "api_key": self.KEY,
             "auth_token": self.TOKEN,
@@ -40,33 +42,36 @@ class RtmApi(object):
         # self.perms = perms
         # self.api_version = api_version
         # self.http = httplib2.Http()
-        
+
     def check_token(self):
+        """docstring"""
         data = self.BASE_HEADER.copy()
         data.update({"api_sig": self._sign_request(data)})
 
         return requests.get(self.AUTH_URL, params=data)
-        
-    def get(self, method:str=None, params:dict={}):
+
+    def get(self, method: str = None, params: dict = {}):
+        """docstring"""
         data = {
             "method": method,
             **self.BASE_HEADER,
             **params
         }
         data.update({"api_sig": self._sign_request(data)})
-        
+
         r = requests.get(self.BASE_URL, params=data)
         return r.json()
 
-    
-    def _sign_request(self, params:dict):
+    def _sign_request(self, params: dict):
+        """docstring"""
         param_pairs = list(params.items())
         param_pairs.sort()
-        param_string = ''.join([k+v for k,v in param_pairs if v is not None])
-        
+        param_string = ''.join([k+v for k, v in param_pairs if v is not None])
+
         secret_string = self.SECRET + param_string
         return hashlib.md5(secret_string.encode('utf-8')).hexdigest()
-        
+
+
 LOCAL_ENVS = './.env'
 dotenv.load_dotenv(LOCAL_ENVS)
 
@@ -75,6 +80,3 @@ rtm = RtmApi(logger)
 
 res = rtm.check_token()
 rtm.BASE_HEADER
-
-
-
