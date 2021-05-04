faf90a6a2f285ae769d77c39d2bbb4139542393b
Alastair Garner <alastairgarner@outlook.com>
2021-05-04 22:02:22 +0100

    adds desktop authentication

--- test_rtm_api.py
+++ test_rtm_api.py
@@ -8,6 +8,8 @@ import requests
 import logging
 from logging import Logger
 
+from rtmcopy import Rtm
+
 logger = logging.getLogger()
 
 app = Flask(__name__)
@@ -22,7 +24,7 @@ class RtmApi(object):
 
     _required_env = ["RTM_API_KEY", "RTM_API_SECRET"]
 
-    def __init__(self, logger: Logger, env_file: str = None) -> None:
+    def __init__(self, logger: Logger, env_file: str = './.env') -> None:
 
         for env in self._required_env:
             if env not in os.environ:
@@ -34,6 +36,10 @@ class RtmApi(object):
         self.SECRET = os.environ["RTM_API_SECRET"]
         self.TOKEN = os.environ["RTM_API_TOKEN"]
 
+        self.frob = None
+        self.timeline = None
+        self.env = env_file
+
         self.BASE_HEADER = {
             "api_key": self.KEY,
             "auth_token": self.TOKEN,
@@ -43,14 +49,7 @@ class RtmApi(object):
         # self.api_version = api_version
         # self.http = httplib2.Http()
 
-    def check_token(self):
-        """docstring"""
-        data = self.BASE_HEADER.copy()
-        data.update({"api_sig": self._sign_request(data)})
-
-        return requests.get(self.AUTH_URL, params=data)
-
-    def get(self, method: str = None, params: dict = {}):
+    def get(self, method: str = None, params: dict = {}, json=True):
         """docstring"""
         data = {
             "method": method,
@@ -60,7 +59,85 @@ class RtmApi(object):
         data.update({"api_sig": self._sign_request(data)})
 
         r = requests.get(self.BASE_URL, params=data)
-        return r.json()
+        return (r.json() if json else r)
+
+    def post(self, method: str = None, params: dict = {}, json=True):
+        """docstring"""
+        data = {
+            "method": method,
+            **self.BASE_HEADER,
+            **params
+        }
+        data.update({"api_sig": self._sign_request(data)})
+
+        r = requests.post(self.BASE_URL, params=data)
+        return (r.json() if json else r)
+
+    def check_token(self):
+        """docstring"""
+
+        return self.post("rtm.auth.checkToken", json=False)
+
+    def get_frob(self):
+        """docstring"""
+
+        rsp = self.get("rtm.auth.getFrob")
+        self.frob = rsp['rsp']['frob']
+
+        return self.frob
+
+    def get_authenication_url(self, frob: str = None, perms: str = "delete"):
+        """docstring"""
+        data = {
+            "api_key": self.KEY,
+            "perms": perms,
+            "frob": frob
+        }
+        data.update({"api_sig": self._sign_request(data)})
+
+        req = requests.Request(method='GET', url=self.AUTH_URL, params=data)
+        r = req.prepare()
+        return r.url
+
+    def get_token(self, frob: str = None):
+        """docstring"""
+
+        if frob is None:
+            frob = self.frob
+
+        return self.get("rtm.auth.getToken", params={"frob": frob}, json=False)
+
+    def authenticate_desktop(self):
+        """docstring"""
+
+        frob = self.get_frob()
+        url = self.get_authenication_url(frob)
+
+        print(
+            f"Follow the URL to authenticate with the Remember The Milk API:\n\n{url}\n")
+
+        confirm = input("Confirm that you've authenticated (Y/n) \n")
+        if confirm.lower() != 'y':
+            print("Failure to authenicate with RTM may result in functionality problems")
+            return
+
+        rsp = self.get_token(frob)
+        if rsp.status_code == 200:
+            success = True
+        else:
+            success = False
+            raise Exception('Failed to get API token')
+
+        self.TOKEN = rsp.json()['rsp']['auth']['token']
+        self._update_dotenv()
+
+        return success
+
+    def _update_dotenv(self):
+        """docstring"""
+
+        dotenv.set_key(self.env, "RTM_API_TOKEN", self.TOKEN)
+        os.environ["RTM_API_TOKEN"] = self.TOKEN
 
     def _sign_request(self, params: dict):
         """docstring"""
@@ -71,6 +148,14 @@ class RtmApi(object):
         secret_string = self.SECRET + param_string
         return hashlib.md5(secret_string.encode('utf-8')).hexdigest()
 
+    def create_timeline(self, **kwargs):
+        """docstring"""
+
+        rsp = self.get('rtm.timelines.create', **kwargs)
+        self.timeline = rsp.json()['rsp']['timeline']
+
+        return self.timeline
+
 
 LOCAL_ENVS = './.env'
 dotenv.load_dotenv(LOCAL_ENVS)
@@ -79,4 +164,13 @@ logger = logging.getLogger()
 rtm = RtmApi(logger)
 
 res = rtm.check_token()
-rtm.BASE_HEADER
+if res.status_code != 200:
+    success = rtm.authenticate_desktop()
+
+res = rtm.create_timeline()
+res['rsp']['timeline']
+
+rtm.post("rtm.tasks.add", )
+
+
+task_list = rtm.get('rtm.tasks.getList')
