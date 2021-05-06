647df6665d48888e8dacba98f56052a4d586c567
Alastair Garner <alastairgarner@outlook.com>
2021-05-06 22:04:30 +0100

    adds `create_task` method

--- test_rtm_api.py
+++ test_rtm_api.py
@@ -5,6 +5,8 @@ import dotenv
 import hashlib
 import requests
 
+from typing import Union
+
 import logging
 from logging import Logger
 
@@ -15,6 +17,105 @@ logger = logging.getLogger()
 app = Flask(__name__)
 
 # NOTE for API call to work, need to pass in the api_key and auth_token
+
+
+class RtmTask(object):
+
+    _fields = {
+        "series_id": 'id',
+        'created': 'created',
+        'modified': 'modified',
+        'name': 'name',
+        'url': 'url',
+        'tags': 'tags',
+        'participants': 'participants',
+        'notes': 'notes',
+    }
+
+    _task_fields = {
+        'id': 'id',
+        'due': 'due',
+        'added': 'added',
+        'completed': 'completed',
+        "deleted": "deleted",
+        "priority": "priority",
+        "postponed": "postponed",
+        "estimate": "estimate",
+    }
+
+    _smart_symbols = {
+        'priority': '!',
+        'due': '^',
+        'start': '~',
+        'tags': '#',
+        'estimate': '=',
+        'location': '@',
+        'url': '',
+        'note': '//',
+    }
+
+    def __init__(self,
+                 name: str = "",
+                 due: str = "",
+                 start: str = "",
+                 priority: Union[str, int] = "",
+                 estimate: str = "",
+                 url: str = "",
+                 tags: Union[str, list] = [],
+                 participants: str = "",
+                 note: str = "",
+                 dictionary: dict = None
+                 ) -> None:
+
+        if dictionary:
+            self.from_dict(dictionary)
+            return None
+
+        if not isinstance(tags, list):
+            tags = [tags]
+
+        self.name = name
+        self.due = due
+        self.start = start
+        self.priority = str(priority)
+        self.estimate = estimate
+        self.url = url
+        self.tags = tags
+        self.participants = participants
+        self.note = note
+
+    def from_dict(self, dictionary: dict = None):
+
+        for attr, key in self._fields.items():
+            val = None
+            if key in dictionary.keys():
+                val = dictionary[key]
+
+            setattr(self, attr, val)
+
+        task_props = dictionary['task'][0]
+        for attr, key in self._task_fields.items():
+            val = None
+            if key in task_props.keys():
+                val = task_props[key]
+
+            setattr(self, attr, val)
+
+    def create_smart_string(self):
+
+        smart_parts = [self.name]
+
+        for attr, symbol in self._smart_symbols.items():
+            value = getattr(self, attr, None)
+
+            if isinstance(value, list):
+                for item in value:
+                    smart_parts.append(symbol + item)
+
+            elif value:
+                smart_parts.append(symbol + value)
+
+        return ' '.join(smart_parts)
 
 
 class RtmApi(object):
@@ -157,47 +258,14 @@ class RtmApi(object):
 
         return self.timeline
 
+    def create_task(self, *args, **kwargs):
+        task = RtmTask(*args, **kwargs)
+        data = {
+            "name": task.create_smart_string(),
+            "parse": "1"
+        }
 
-class RtmTask(object):
-
-    _fields = {
-        "series_id": 'id',
-        'created': 'created',
-        'modified': 'modified',
-        'name': 'name',
-        'url': 'url',
-        'tags': 'tags',
-        'participants': 'participants',
-        'notes': 'notes',
-    }
-
-    _task_fields = {
-        'id': 'id',
-        'due': 'due',
-        'added': 'added',
-        'completed': 'completed',
-        "deleted": "deleted",
-        "priority": "priority",
-        "postponed": "postponed",
-        "estimate": "estimate",
-    }
-
-    def __init__(self, task_dict) -> None:
-
-        for attr, key in self._fields.items():
-            val = None
-            if key in task_dict.keys():
-                val = task_dict[key]
-
-            setattr(self, attr, val)
-
-        task_props = task_dict['task'][0]
-        for attr, key in self._task_fields.items():
-            val = None
-            if key in task_props.keys():
-                val = task_props[key]
-
-            setattr(self, attr, val)
+        self.post("rtm.tasks.add", data)
 
 
 LOCAL_ENVS = './.env'
@@ -211,19 +279,16 @@ if res.status_code != 200:
     success = rtm.authenticate_desktop()
 
 res = rtm.create_timeline()
-res['rsp']['timeline']
+res
 
-data = {
-    "name": "Cuddle Kat !1 ^today",
-    "parse": "1"
-}
-res = rtm.post("rtm.tasks.add", data)
+task = rtm.create_task(name="Cuddle Kat", priority=1, due='today',
+                       tags=['work', 'home'], url='https://address.com', note='This is a long note pertaining to the thing I have to do')
 
 
 task_list = rtm.get('rtm.tasks.getList')
 d = task_list['rsp']['tasks']['list'][0]['taskseries'][0]
 
-task = RtmTask(d)
+task = RtmTask(dictionary=d)
 d['task']
 
 task.id
