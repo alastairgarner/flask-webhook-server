7735f7a0ea21f80b9b7203360916e30e462f38aa
Alastair Garner <garneralastair@gmail.com>
2021-05-24 20:55:22 +0100

    heroku env vars test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from flask import Flask, Response
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.logger.setLevel('DEBUG')
 @app.route('/')
 def hello_world():
     app.logger.info('Home screen viewed')
+    app.logger.info(f'This is my demo environment variable - {os.environ['DEMO_TOKEN']}')
     return Response('This is the welcome screen, hosted on Heroku', 200)
 
 
