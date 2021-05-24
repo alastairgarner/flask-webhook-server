805ae8850989580a0a1d16b93932c143a5055120
Alastair Garner <garneralastair@gmail.com>
2021-05-24 20:58:32 +0100

    fixed f-string issue

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app.logger.setLevel('DEBUG')
 @app.route('/')
 def hello_world():
     app.logger.info('Home screen viewed')
-    app.logger.info(f'This is my demo environment variable - {os.environ['DEMO_TOKEN']}')
+    app.logger.info(
+        f"This is my demo environment variable - {os.environ['DEMO_TOKEN']}")
     return Response('This is the welcome screen, hosted on Heroku', 200)
 
 
