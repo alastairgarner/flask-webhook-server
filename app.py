8ae57bc2bfc8cd5d40e0953232a58d88c80874d7
Alastair Garner <garneralastair@gmail.com>
2021-05-24 21:04:35 +0100

    trial webhhook on heroku

--- app.py
+++ app.py
@@ -2,16 +2,34 @@
 import os
 from flask import Flask, Response
 
+from flask_webhook_server import BaseWebhook, GithubWebhook, RtmApi, BasePacket
+import dotenv
+
+try:
+    LOCAL_ENVS = './.env'
+    dotenv.load_dotenv(LOCAL_ENVS)
+except:
+    pass
+
 app = Flask(__name__)
 app.logger.setLevel('DEBUG')
+
+github = GithubWebhook(app, app.logger)
+rtmilk = RtmApi(app, app.logger)
+
+github.register(event=('star', 'created'), function=rtmilk.yeah_boi)
 
 
 @app.route('/')
 def hello_world():
     app.logger.info('Home screen viewed')
-    app.logger.info(
-        f"This is my demo environment variable - {os.environ['DEMO_TOKEN']}")
-    return Response('This is the welcome screen, hosted on Heroku', 200)
+    return Response('This is the welcome screen', 200)
+
+
+@github.hook('/github', methods=['POST', 'GET'])
+def on_github_post(packet):
+    # app.logger.info('Running the github response')
+    return None
 
 
 if __name__ == "__main__":
